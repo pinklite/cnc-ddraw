4a45d7942bdeaa607b6e3f198bf4be2b62695ecf
FunkyFr3sh <cc.red.alert.1@googlemail.com>
2022-09-21 16:06:17 +0200

    filter WM_TIMER

--- src/wndproc.c
+++ src/wndproc.c
@@ -19,7 +19,7 @@ LRESULT CALLBACK fake_WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam
 #ifdef _DEBUG_X
     if (uMsg != WM_MOUSEMOVE && uMsg != WM_NCMOUSEMOVE && uMsg != WM_NCHITTEST && uMsg != WM_SETCURSOR &&
         uMsg != WM_KEYUP && uMsg != WM_KEYDOWN && uMsg != WM_CHAR && uMsg != WM_DEADCHAR && uMsg != WM_INPUT &&
-        uMsg != WM_UNICHAR && uMsg != WM_IME_CHAR && uMsg != WM_IME_KEYDOWN && uMsg != WM_IME_KEYUP)
+        uMsg != WM_UNICHAR && uMsg != WM_IME_CHAR && uMsg != WM_IME_KEYDOWN && uMsg != WM_IME_KEYUP && uMsg != WM_TIMER)
     {
         TRACE_EXT(
             "     uMsg = %s (%d), wParam = %08X (%d), lParam = %08X (%d)\n",
