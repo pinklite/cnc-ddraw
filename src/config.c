e4d6f712a4ac4a75a14f352da76d5baf31825c3f
FunkyFr3sh <cc.red.alert.1@googlemail.com>
2022-10-15 21:30:14 +0200

    set catmull-rom-bilinear as the new default shader

--- src/config.c
+++ src/config.c
@@ -151,7 +151,7 @@ void cfg_load()
     }
 
     /* to do: read .glslp config file instead of the shader and apply the correct settings  */
-    cfg_get_string("shader", "", g_ddraw->shader, sizeof(g_ddraw->shader));
+    cfg_get_string("shader", "Shaders\\cubic\\catmull-rom-bilinear.glsl", g_ddraw->shader, sizeof(g_ddraw->shader));
 
     cfg_get_string("renderer", "auto", tmp, sizeof(tmp));
     TRACE("     Using %s renderer\n", tmp);
@@ -276,7 +276,7 @@ static void cfg_create_ini()
             "\n"
             "; Preliminary libretro shader support - (Requires 'renderer=opengl') https://github.com/libretro/glsl-shaders\n"
             "; 2x scaling example: https://imgur.com/a/kxsM1oY - 4x scaling example: https://imgur.com/a/wjrhpFV\n"
-            "shader=Shaders\\interpolation\\bilinear.glsl\n"
+            "shader=Shaders\\cubic\\catmull-rom-bilinear.glsl\n"
             "\n"
             "; Window position, -32000 = center to screen\n"
             "posX=-32000\n"
