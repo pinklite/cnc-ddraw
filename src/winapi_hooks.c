67188ea5001f979c45ce5ce06df457074ad61789
FunkyFr3sh <cc.red.alert.1@googlemail.com>
2023-02-27 18:30:35 +0100

    add NULL check

--- src/winapi_hooks.c
+++ src/winapi_hooks.c
@@ -557,7 +557,7 @@ BOOL WINAPI fake_PeekMessageA(LPMSG lpMsg, HWND hWnd, UINT wMsgFilterMin, UINT w
 {
     BOOL result = real_PeekMessageA(lpMsg, hWnd, wMsgFilterMin, wMsgFilterMax, wRemoveMsg);
 
-    if (result && g_ddraw->hook_peekmessage)
+    if (result && g_ddraw && g_ddraw->hook_peekmessage)
     {
         switch (lpMsg->message)
         {
